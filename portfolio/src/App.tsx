f9c5a200641dc18fbabd5d62f77fb94230a5d9f9
신종혁 <shinjh4903@gmail.com>
2023-12-17 16:06:38 +0900

    🎉 프로젝트 세팅

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>포트폴리오</div>
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ portfolio/src/styles/DefaultTheme.ts
@@ -0,0 +1,31 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    main: "#4A90E2",
+    light: "#9ECDF2",
+    dark: "#2D51A0",
+    white: "#F1F7FD",
+    black: "#1D2C4E",
+    lightgray: "#323248",
+    gray: "rgba(0, 0, 0, 0.50)",
+    night: "#000020",
+    menu: "#E0ECED",
+  },
+  fonts: {
+    lightfont: "lightfont",
+    regularfont: "regularfont",
+    semiboldfont: "semiboldfont",
+    boldfont: "boldfont",
+  },
+  fontsize: {
+    xsmall: "12px",
+    small: "14px",
+    regular: "16px",
+    large: "18px",
+    xlarge: "20px",
+    xxlarge: "24px",
+    xxxlarge: "34px",
+  },
+};
+export default theme;
